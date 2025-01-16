6aae2eac2795ff1a8a7b5dea0c20b596d8a14120
Aaron Worsham <aaronworsham@gmail.com>
2025-01-16 08:49:14 -0500

    backup

---
+++ dist/src/api/dialog/services/dialog.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * dialog service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::dialog.dialog');
